9d6a30e53267623dcfd427505ed385ded18b5ca6
Rodrigo de Andrade <79064228+RodrigodeAndrade90@users.noreply.github.com>
2025-11-03 20:38:11 -0300

    Volta do connection.ts

--- backend/src/routes/esp32/connection.ts
+++ backend/src/routes/esp32/connection.ts
@@ -1,22 +1,26 @@
-// routes/connection.ts
 import { Router } from "express";
 import { enviarParaESP } from '../../controllers/esp32Controller';
 import { 
-  registrarQueda, 
   getHistorico, 
   getUltimaQueda 
 } from '../../controllers/quedaController';
 
 const router = Router();
 
-// Rota para receber dados de queda do ESP32
-router.post("/queda", registrarQueda);
+router.post("/queda", (req, res) => {
+  console.log("📡 Dados recebidos:", req.body);
+  res.json({ status: "ok", recebido: req.body });
+
+  
+});
 
 // Rotas para o aplicativo React Native
 router.get("/queda/historico", getHistorico);
 router.get("/queda/ultima", getUltimaQueda);
 
+
 // Bruno Menezes 01.11.2025: Rota para enviar dados para ESP32
 router.post('/enviar', enviarParaESP);
+  
 
 export default router;
